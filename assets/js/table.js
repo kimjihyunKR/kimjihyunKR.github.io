5491dbcff0ededfeddd4a2c7ac625fe2ce58cd24
kimjihyunKR <kkkk6810@on.mjc.ac.kr>
2021-02-14 03:09:25 +0900

    210214

--- assets/js/table.js
+++ assets/js/table.js
@@ -1,3 +1,25 @@
+const tableWraper = document.querySelector('.table-content');
+
+// const tableOfHeaders = document.querySelectorAll('.post-content > h1, h2, h3');
 const tableOfHeader1 = document.querySelectorAll('.post-content h1');
-const tableOfHeader2 = document.querySelectorAll('.post-content h2');
-console.log(tableOfHeader1);
+// console.log(tableOfHeaders);
+
+function replaceSymbolChar(text){
+  text = text.replace( / /g , "-");
+  text = text.replace( /\./g , "");
+  text = text.replace( /,/g , "");
+  text = text.replace( /!/g , "");
+  text = text.replace( /\?/g , "");
+  return text;
+}
+
+tableOfHeader1.forEach( title => {
+  const li = document.createElement('li');
+  const anchor = document.createElement('a');
+  // 특수문자 처리
+  let headerId = replaceSymbolChar(title.innerText);
+  anchor.href = '#' +  headerId;
+  anchor.innerText = title.innerText;
+  li.append(anchor);
+  tableWraper.append(li);
+});
